8bdaebaeb02b2bb004f1c092044028aca2f83431
Hevelmo <hevelmo060683@gmail.com>
2018-03-07 10:22:38 -0600

    commit

---
+++ estructura/lib/core.lib.js
@@ -0,0 +1,1 @@
+//@prepros-append assets/jquery/v3.3.1/jquery-3.3.1.js
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="es-MX" class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="es-MX" class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="es-MX" class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html lang="es-MX" class="no-js lt-ie10"> <![endif]-->
+<html class="no-js" lang="es">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=10">
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE10">
+        <meta http-equiv="X-UA-Compatible" content="IE=9">
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9">
+        <meta http-equiv="X-UA-Compatible" content="IE=8">
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
+        <meta http-equiv="X-UA-Compatible" content="IE=5">
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta http-equiv="cache-control" value="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="expires" content="0">
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=5.0"/>
+
+        <title>Login</title>
+
+        <link id="main" rel="stylesheet" class="link-main"  rel="stylesheet" href="../assets/css/core.css" media="screen">
+        <?php /* 
+        <link id="main" rel="stylesheet" class="link-main"  rel="stylesheet" href="../assets/css/min/core.min.css">
+        */ ?>
+    </head>
+    <body>
+        
+        <script src="../assets/lib/min/core.lib.min.js"></script>
+        <script src="../assets/js/min/core.min.js"></script>
+    </body>
+</html>
